51a2f2e1b14e5f40d5eb8cdacef7f927df59ac44
abdulrahmanshr75 <94563329+abdulrahmanshr75@users.noreply.github.com>
2022-01-20 21:52:13 +0200

    update linters

--- src/crud.js
+++ src/crud.js
@@ -5,7 +5,7 @@ export default class Todo {
     const index = this.todos.length + 1;
     const completed = false;
     this.todos.push({ description, completed, index });
-    localStorage.setItem("todos", JSON.stringify(this.todos));
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   };
 
   editTheIndex = () => {
@@ -19,12 +19,12 @@ export default class Todo {
   deleteTodo = (index) => {
     this.todos = this.todos.filter((item) => Number(index) !== item.index);
     this.editTheIndex();
-    localStorage.setItem("todos", JSON.stringify(this.todos));
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   };
 
   getFromLocalStorage = () => {
-    if (localStorage.getItem("todos")) {
-      this.todos = JSON.parse(localStorage.getItem("todos"));
+    if (localStorage.getItem('todos')) {
+      this.todos = JSON.parse(localStorage.getItem('todos'));
     }
     return this.todos;
   };
@@ -32,13 +32,13 @@ export default class Todo {
   updateTodo = (index, description) => {
     const todo = this.todos.find((item) => Number(index) === item.index);
     todo.description = description;
-    localStorage.setItem("todos", JSON.stringify(this.todos));
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   };
 
   done = () => {
     const uncompleted = this.todos.filter((item) => item.completed !== true);
     this.editTheIndex();
-    localStorage.setItem("todos", JSON.stringify(uncompleted));
+    localStorage.setItem('todos', JSON.stringify(uncompleted));
   };
 
   isCompleted = (index) => {
@@ -48,18 +48,18 @@ export default class Todo {
     } else {
       todo.completed = true;
     }
-    localStorage.setItem("todos", JSON.stringify(this.todos));
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   };
 
   checkingTodo = () => {
-    const checkbox = document.querySelectorAll(".check");
+    const checkbox = document.querySelectorAll('.check');
     checkbox.forEach((item) => {
       if (item.checked === true) {
         item.completed = false;
       } else {
         item.completed = true;
       }
-      localStorage.setItem("todos", JSON.stringify(this.checkbox));
+      localStorage.setItem('todos', JSON.stringify(this.checkbox));
     });
   };
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import "./style.css";
-import Todo from "./crud.js";
+import './style.css';
+import Todo from './crud.js';
 
 const todo = new Todo();
 const displayTodo = (todo, checked) => `<li class="todo-added">
@@ -33,36 +33,36 @@ const block = () => `<section class="main-container">
     </section>`;
 
 const ischecked = (item) => {
-  if (item === true) return "checked";
+  if (item === true) return 'checked';
 };
 
-const main = document.querySelector("main");
+const main = document.querySelector('main');
 main.innerHTML = block();
 
-const todos = document.querySelector(".todos");
+const todos = document.querySelector('.todos');
 const structure = () => {
-  todos.innerHTML = "";
+  todos.innerHTML = '';
   todo.getFromLocalStorage().forEach((item) => {
     todos.innerHTML += displayTodo(item, ischecked(item.completed));
   });
-  const deleteTodo = document.querySelectorAll(".delete");
+  const deleteTodo = document.querySelectorAll('.delete');
   deleteTodo.forEach((item) => {
-    item.addEventListener("click", () => {
-      todo.deleteTodo(item.getAttribute("value"));
+    item.addEventListener('click', () => {
+      todo.deleteTodo(item.getAttribute('value'));
       structure();
     });
   });
 
-  const textLined = document.querySelectorAll(".text-lined");
+  const textLined = document.querySelectorAll('.text-lined');
   textLined.forEach((item) => {
-    item.addEventListener("input", () => {
-      todo.updateTodo(item.getAttribute("data-index"), item.innerHTML);
+    item.addEventListener('input', () => {
+      todo.updateTodo(item.getAttribute('data-index'), item.innerHTML);
     });
   });
 
-  const select = document.querySelectorAll(".check");
+  const select = document.querySelectorAll('.check');
   select.forEach((item) => {
-    item.addEventListener("change", () => {
+    item.addEventListener('change', () => {
       todo.isCompleted(item.value);
     });
   });
@@ -70,16 +70,16 @@ const structure = () => {
 
 structure();
 
-const form = document.querySelector(".todo-form");
-form.addEventListener("submit", (e) => {
+const form = document.querySelector('.todo-form');
+form.addEventListener('submit', (e) => {
   e.preventDefault();
   todo.createTodo(form.elements.description.value);
   form.reset();
   structure();
 });
 
-const completed = document.querySelector(".clear");
-completed.addEventListener("click", (e) => {
+const completed = document.querySelector('.clear');
+completed.addEventListener('click', (e) => {
   e.preventDefault();
   todo.done();
   structure();
