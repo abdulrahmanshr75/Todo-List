7870709b6693e17af03436a54ab30fcce45b4494
abdulrahmanshr75 <94563329+abdulrahmanshr75@users.noreply.github.com>
2022-01-26 15:03:27 +0200

    updates

--- src/crud.test.js
+++ src/crud.test.js
@@ -7,21 +7,3 @@ describe("Create Todo", () => {
     todo.createTodo("todo1");
     expect(todo.todos.length).toBe(1);
   });
-
-  test("check the descr", () => {
-    todo.createTodo("test-2");
-    expect(todo.todos[1].description).toBe("test-2");
-  });
-  test("check the descr", () => {
-    todo.createTodo("test-3");
-    expect(todo.todos.length).toBe(3);
-  });
-});
-
-describe("deleteTodo", () => {
-  it("remove todo", () => {
-    const index = 1;
-    todo.deleteTodo(index);
-    expect(todo.todos.length).toBe(2);
-  });
-});
