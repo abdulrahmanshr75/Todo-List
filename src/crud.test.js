ac71a6bef289c8f86aab37925190500124102ac8
abdulrahmanshr75 <94563329+abdulrahmanshr75@users.noreply.github.com>
2022-01-27 13:12:23 +0200

    implement functionalty

--- src/crud.test.js
+++ src/crud.test.js
@@ -1,27 +1,79 @@
 /** * @jest-environment jsdom */
-import Todo from './crud.js';
+import Todo from "./crud.js";
+jest.mock("./localStorageMock.js");
 
-jest.mock('./localStorageMock');
 const todo = new Todo();
-describe('Create Todo', () => {
-  test('creating todo', () => {
-    todo.createTodo('Todo-1');
+describe("Create Todo", () => {
+  test("creating todo", () => {
+    todo.createTodo("Todo-1");
     expect(todo.todos.length).toBe(1);
   });
-  test('check the description', () => {
-    todo.createTodo('Todo-2');
-    expect(todo.todos[1].description).toBe('Todo-2');
+  test("check the description", () => {
+    todo.createTodo("Todo-2");
+    expect(todo.todos[1].description).toBe("Todo-2");
   });
-  test('check the description', () => {
-    todo.createTodo('Todo-3');
+  test("check the description", () => {
+    todo.createTodo("Todo-3");
     expect(todo.todos.length).toBe(3);
   });
 });
 
-describe('deleteTodo', () => {
-  it('remove todo', () => {
+describe("deleteTodo", () => {
+  it("remove todo", () => {
     const index = 1;
     todo.deleteTodo(index);
+    expect(todo.todos.length).toBe(2);
+  });
+});
+/* testing part 2 */
+describe("Edit Todo", () => {
+  const index = 1;
+  it("Changing the text from 'Todo-2' to 'Hello-World'", () => {
+    todo.updateTodo(index, "Hello-World");
+    expect(
+      todo.todos
+        .map((item) => {
+          if (item.index === index) {
+            return item.description;
+          }
+          return null;
+        })
+        .join("")
+    ).toBe("Hello-World");
+  });
+  it("It should change the status from false to true", () => {
+    const index = 1;
+    todo.isCompleted(index);
+    expect(
+      todo.todos
+        .map((item) => {
+          if (item.index === index) {
+            return item.completed;
+          }
+          return null;
+        })
+        .join("")
+    ).toBeTruthy();
+  });
+  it("It should change the status from true to false", () => {
+    const index = 1;
+    todo.isCompleted(index);
+    expect(
+      todo.todos
+        .map((item) => {
+          if (item.index === Number(index)) {
+            return item.completed;
+          }
+          return null;
+        })
+        .join("")
+    ).toMatch("false");
+  });
+});
+
+describe("Clear all todos", () => {
+  it("It should clear all the todos", () => {
+    todo.done();
     expect(todo.todos.length).toBe(2);
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import './style.css';
-import Todo from './crud.js';
+import "./style.css";
+import Todo from "./crud.js";
 
 const todo = new Todo();
-const displayTodo = (todo, checked) => `<li class="todo-added">
+export const displayTodo = (todo, checked) => `<li class="todo-added">
                     <div>
                     <input ${checked} class="check" type="checkbox" value="${todo.index}"/>
                      <span class="text-lined" data-index="${todo.index}" contentEditable="true"> ${todo.description} </span>
@@ -15,7 +15,7 @@ const displayTodo = (todo, checked) => `<li class="todo-added">
                     </div>
                 </li>`;
 
-const block = () => `<section class="main-container">
+export const block = () => `<section class="main-container">
         <div>
             <div class="heading">
                 <h2 class="todays"> Today's To Do</h2>
@@ -33,36 +33,36 @@ const block = () => `<section class="main-container">
     </section>`;
 
 const ischecked = (item) => {
-  if (item === true) return 'checked';
+  if (item === true) return "checked";
 };
 
-const main = document.querySelector('main');
+const main = document.querySelector("main");
 main.innerHTML = block();
 
-const todos = document.querySelector('.todos');
-const structure = () => {
-  todos.innerHTML = '';
+const todos = document.querySelector(".todos");
+export const structure = () => {
+  todos.innerHTML = "";
   todo.getFromLocalStorage().forEach((item) => {
     todos.innerHTML += displayTodo(item, ischecked(item.completed));
   });
-  const deleteTodo = document.querySelectorAll('.delete');
+  const deleteTodo = document.querySelectorAll(".delete");
   deleteTodo.forEach((item) => {
-    item.addEventListener('click', () => {
-      todo.deleteTodo(item.getAttribute('value'));
+    item.addEventListener("click", () => {
+      todo.deleteTodo(item.getAttribute("value"));
       structure();
     });
   });
 
-  const textLined = document.querySelectorAll('.text-lined');
+  const textLined = document.querySelectorAll(".text-lined");
   textLined.forEach((item) => {
-    item.addEventListener('input', () => {
-      todo.updateTodo(item.getAttribute('data-index'), item.innerHTML);
+    item.addEventListener("input", () => {
+      todo.updateTodo(item.getAttribute("data-index"), item.innerHTML);
     });
   });
 
-  const select = document.querySelectorAll('.check');
+  const select = document.querySelectorAll(".check");
   select.forEach((item) => {
-    item.addEventListener('change', () => {
+    item.addEventListener("change", () => {
       todo.isCompleted(item.value);
     });
   });
@@ -70,16 +70,16 @@ const structure = () => {
 
 structure();
 
-const form = document.querySelector('.todo-form');
-form.addEventListener('submit', (e) => {
+const form = document.querySelector(".todo-form");
+form.addEventListener("submit", (e) => {
   e.preventDefault();
   todo.createTodo(form.elements.description.value);
   form.reset();
   structure();
 });
 
-const completed = document.querySelector('.clear');
-completed.addEventListener('click', (e) => {
+const completed = document.querySelector(".clear");
+completed.addEventListener("click", (e) => {
   e.preventDefault();
   todo.done();
   structure();
