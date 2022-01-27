67752dd8da1c14bef47e8f560495c837dda7c4f2
abdulrahmanshr75 <94563329+abdulrahmanshr75@users.noreply.github.com>
2022-01-27 14:16:42 +0200

    update crud

--- src/crud.test.js
+++ src/crud.test.js
@@ -1,8 +1,8 @@
 /** * @jest-environment jsdom */
 import Todo from "./crud.js";
 import { block, displayTodo } from "./domMock.js";
-
 jest.mock("./localStorageMock.js");
+
 const todo = new Todo();
 describe("Create Todo", () => {
   test("creating todo", () => {
@@ -27,72 +27,3 @@ describe("deleteTodo", () => {
   });
 });
 /* testing part 2 */
-describe("Edit Todo", () => {
-  const index = 1;
-  test("Changing the text from 'Todo-2' to 'Hello-World'", () => {
-    todo.updateTodo(index, "Hello-World");
-    expect(
-      todo.todos
-        .map((item) => {
-          if (item.index === index) {
-            return item.description;
-          }
-          return null;
-        })
-        .join("")
-    ).toBe("Hello-World");
-  });
-  test("It should change the status from false to true", () => {
-    const index = 1;
-    todo.isCompleted(index);
-    expect(
-      todo.todos
-        .map((item) => {
-          if (item.index === index) {
-            return item.completed;
-          }
-          return null;
-        })
-        .join("")
-    ).toBeTruthy();
-  });
-  test("It should change the status from true to false", () => {
-    const index = 1;
-    todo.isCompleted(index);
-    expect(
-      todo.todos
-        .map((item) => {
-          if (item.index === Number(index)) {
-            return item.completed;
-          }
-          return null;
-        })
-        .join("")
-    ).toMatch("false");
-  });
-});
-
-describe("Clear all todos", () => {
-  test("It should clear all the todos", () => {
-    todo.done();
-    expect(todo.todos.length).toBe(2);
-  });
-});
-
-describe("DOM manipulation", () => {
-  test("Display the main container", () => {
-    document.body.innerHTML = "<main></main>";
-    const main = document.querySelector("main");
-    main.innerHTML = block();
-    expect(document.querySelectorAll(".main-container").length).toBe(1);
-  });
-  test("Display the todo component", () => {
-    document.body.innerHTML = '<ul class="todos"></ul>';
-    const todos = document.querySelectorAll(".todos");
-    todo.todos.forEach((item) => {
-      todos.innerHTML += displayTodo(item);
-    });
-    const list = document.querySelectorAll(".todos");
-    expect(list).toHaveLength(1);
-  });
-});
