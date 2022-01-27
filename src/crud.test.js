ae60177c0fa2df5db0b01a6dc30e6dc0a7d69a62
Benjamin Semah <benjaminsemah@gmail.com>
2022-01-27 12:21:30 +0000

    Add second part of testing

--- src/crud.test.js
+++ src/crud.test.js
@@ -26,4 +26,50 @@ describe("deleteTodo", () => {
     expect(todo.todos.length).toBe(2);
   });
 });
-/* testing part 2 */
+
+
+
+describe("Edit Todo", () => {
+  const index = 1;
+  test("Changing the text from 'Todo-2' to 'Hello-World'", () => {
+    todo.updateTodo(index, "Hello-World");
+    expect(
+      todo.todos
+        .map((item) => {
+          if (item.index === index) {
+            return item.description;
+          }
+          return null;
+        })
+        .join("")
+    ).toBe("Hello-World");
+  });
+  test("It should change the status from false to true", () => {
+    const index = 1;
+    todo.isCompleted(index);
+    expect(
+      todo.todos
+        .map((item) => {
+          if (item.index === index) {
+            return item.completed;
+          }
+          return null;
+        })
+        .join("")
+    ).toBeTruthy();
+  });
+  test("It should change the status from true to false", () => {
+    const index = 1;
+    todo.isCompleted(index);
+    expect(
+      todo.todos
+        .map((item) => {
+          if (item.index === Number(index)) {
+            return item.completed;
+          }
+          return null;
+        })
+        .join("")
+    ).toMatch("false");
+  });
+});
